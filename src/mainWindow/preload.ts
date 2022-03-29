68c825d53fc43ffe27d4c3a789c4c63c5009a887
tiborhari <hartib@gmail.com>
2022-03-29 23:07:09 +0200

    Handle errors from shell.openExternal.

--- src/mainWindow/preload.ts
+++ src/mainWindow/preload.ts
@@ -10,7 +10,13 @@ const api = <const>{
     warn: log.warn,
   },
   onNewState: (callback: Parameters<typeof ipcRenderer.on>[1]) => ipcRenderer.on('newState', callback),
-  openLinkInBrowser: (url: string) => shell.openExternal(url),
+  openLinkInBrowser: (url: string) => {
+    try {
+      shell.openExternal(url);
+    } catch (error) {
+      log.error('Error in shell.openExternal', error);
+    }
+  },
   sendAction: (version: unknown, action: unknown) => ipcRenderer.sendSync('action', version, action),
 };
 
